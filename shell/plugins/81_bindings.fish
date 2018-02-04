dc39d1f914630ce44b11a122cf71a5a1d9e66cc7
Shogun <shogun@cowtech.it>
2018-02-04 15:35:43 +0100

    Updated license links and badges.

--- shell/plugins/81_bindings.fish
+++ shell/plugins/81_bindings.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 #
 # This file is part of fishamnium. Copyright (C) 2013 and above Shogun <shogun@cowtech.it>.
-# Licensed under the MIT license, which can be found at https://choosealicense.com/licenses/mit/.
+# Licensed under the MIT license, which can be found at https://choosealicense.com/licenses/mit.
 #
 
 function fish_user_key_bindings
