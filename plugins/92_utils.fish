42221d4d36ee25e8835cbc9dd31969120a3908d9
Shogun <paolo@cowtech.it>
2022-03-13 07:00:31 +0100

    feat: Allow comments in execute_on_list.

--- plugins/92_utils.fish
+++ plugins/92_utils.fish
@@ -1,9 +1,11 @@
 function execute_on_list
   set orig_pwd $PWD
 
-  for i in (cat $argv[1])
-    echo "--- $i ---"
-    cd $i
+  for item in (cat $argv[1])
+    string match -qr -- "^#" "$item"; and continue;
+
+    echo "--- $item ---"
+    cd $item
     eval $argv[2..]
     cd $orig_pwd
   end
