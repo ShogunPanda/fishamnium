e3b186db72cd7619870cc02e864b3e663701419c
Paolo Insogna <paolo@cowtech.it>
2022-05-31 16:41:36 +0200

    feat: Added nrs and pnrs.

--- plugins/42_npm.fish
+++ plugins/42_npm.fish
@@ -22,3 +22,26 @@ alias pnrl="pnpm run lint"
 alias pnrd="pnpm run deploy"
 alias pno="pnpm outdated"
 alias pnu="pnpm update"
+
+function nrs -d "Interactively run a script using npm"
+  set scripts (yq -o tsv -eMP ".scripts | to_entries | map([.key])" package.json 2>/dev/null)
+  test ! $status -eq 0; and return
+
+  set runner $RUNNER
+  test -z "$runner"; and set runner "npm"
+
+  set prompt "--> Which script do you want to run? "
+  set colors "prompt:3:bold,bg+:-1,fg+:2:bold,pointer:2:bold,hl:-1:underline,hl+:2:bold:underline"
+  set height (math (count $scripts) + 1)
+
+  set choice (string join0 $scripts | fzf --read0 -e --prompt $prompt --info=hidden --preview-window=hidden --height $height --reverse --color $colors)
+  
+  if test $status -eq 0
+    echo -e "\x1b[33m--> $runner run $choice\x1b[0m"
+    $runner run $choice
+  end
+end
+
+function pnrs -d "Interactively run a script using pnpm"
+  RUNNER=pnpm nrs
+end
