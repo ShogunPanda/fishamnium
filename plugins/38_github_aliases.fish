00fa813db868e6ae7feceb2fa579235e674e6b71
Paolo Insogna <paolo@cowtech.it>
2025-02-10 11:01:29 +0100

    feat: Added ghpr.

--- plugins/38_github_aliases.fish
+++ plugins/38_github_aliases.fish
@@ -1,6 +1,7 @@
 # Github
 alias gho='gh repo view --web'
 
+alias ghpr='gh pr list -H (git branch --show-current) --json number -q ".[0].number"'
 alias ghprb=gh_pr_branch
 alias ghpra=gh_pr_approve
 alias ghprm='gh pr merge'
