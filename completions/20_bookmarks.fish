2162b77ae7758d986195a41a3ac19e05d0b5b5f7
Paolo Insogna <paolo@cowtech.it>
2025-05-15 14:11:28 +0200

    fix: Added missing alias completion.

--- completions/20_bookmarks.fish
+++ completions/20_bookmarks.fish
@@ -14,7 +14,7 @@ complete -c bookmarks_list -x -a ""
 complete -c bookmark_save -x -a ""
 
 # All other commands take only the bookmark name as completions
-for i in bookmark_show bookmark_delete bookmark_cd bookmark_open bookmark_edit
+for i in bookmark_show bookmark_delete bookmark_cd bookmark_open bookmark_edit y
   complete -c $i -x -n "__fishamnium_is_bookmark_argument 1" -a "(bookmarks_autocomplete)"
   complete -c $i -x -n "__fishamnium_is_bookmark_argument 2" -a ""
 end
