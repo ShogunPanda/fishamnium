570db872aa5528a6ad66c3d26e2f5723ac7a8b11
Paolo Insogna <paolo@cowtech.it>
2025-08-21 10:05:44 +0200

    feat: New Node aliases.

--- plugins/41_node.fish
+++ plugins/41_node.fish
@@ -1,5 +1,8 @@
-alias nto="node --test-only"
+alias nt="node --test"
+alias ntt="node --test --test-reporter=tap"
+alias nto="node --test --test-only"
+alias ntot="node --test --test-only --test-reporter=tap"
 
 function ntow
-  NODE_OPTIONS=--test-only wtfnode $argv
+  NODE_OPTIONS="--test --test-only" wtfnode $argv
 end
