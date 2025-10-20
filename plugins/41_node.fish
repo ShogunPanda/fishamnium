798e3f5b9ebe35f5c44353bcea70c1aec8e4d1ca
Paolo Insogna <paolo@cowtech.it>
2025-10-20 16:05:00 +0200

    feat: Added single test concurrency aliases for Node.js
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- plugins/41_node.fish
+++ plugins/41_node.fish
@@ -1,8 +1,16 @@
 alias nt="node --test"
+alias nt1="node --test --test-concurrency=1"
 alias ntt="node --test --test-reporter=tap"
+alias ntt1="node --test --test-concurrency=1 --test-reporter=tap"
 alias nto="node --test --test-only"
+alias nto1="node --test --test-concurrency=1 --test-only"
 alias ntot="node --test --test-only --test-reporter=tap"
+alias ntot1="node --test --test-concurrency=1 --test-only --test-reporter=tap"
 
 function ntow
+  NODE_OPTIONS="--test --test-only" wtfnode $argv
+end
+
+function ntow1
   NODE_OPTIONS="--test --test-only" wtfnode $argv
 end
