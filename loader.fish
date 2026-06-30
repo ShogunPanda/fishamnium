b47c43b0c11c979e4d8a5366b01560314860c403
Paolo Insogna <paolo@cowtech.it>
2026-06-30 11:59:16 +0200

    fix: Fixed loading.
    Signed-off-by: Paolo Insogna <paolo@cowtech.it>

--- loader.fish
+++ loader.fish
@@ -1,13 +1,10 @@
 # Set defaults
 set -x -g FISHAMNIUM_VERSION (string trim (string collect < ~/.local/share/fishamnium/version))
 
-test $(count $FISHAMNIUM_PLUGINS) -eq 0; and set -x FISHAMNIUM_PLUGINS (path basename ~/.local/share/fishamnium/plugins/*.fish)
-test $(count $FISHAMNIUM_COMPLETIONS) -eq 0; and set -x FISHAMNIUM_COMPLETIONS (path basename ~/.local/share/fishamnium/completions/*.fish)
-
 # Load plugins files
 set -e -g FISHAMNIUM_LOADED_PLUGINS
-for i in $(string split " " "$FISHAMNIUM_PLUGINS")
-  set source ~/.local/share/fishamnium/plugins/$i
+for source in ~/.local/share/fishamnium/plugins/*.fish
+  set i (path basename "$source")
   
   if test -x $source
     set -x -g FISHAMNIUM_LOADED_PLUGINS $FISHAMNIUM_LOADED_PLUGINS $i
@@ -17,8 +14,8 @@ end
 
 # Load completions files
 set -e -g FISHAMNIUM_LOADED_COMPLETIONS
-for i in $(string split " " "$FISHAMNIUM_COMPLETIONS")
-  set source ~/.local/share/fishamnium/completions/$i
+for source in ~/.local/share/fishamnium/completions/*.fish
+  set i (path basename "$source")
   
   if test -x $source
     set -x -g FISHAMNIUM_LOADED_COMPLETIONS $FISHAMNIUM_LOADED_COMPLETIONS $i
